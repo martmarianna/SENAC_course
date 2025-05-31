920e03d51a54c1cdc956e8a50152a799b68321a6
Marianna Martins <157758786+martmarianna@users.noreply.github.com>
2025-05-30 21:38:12 -0300

    Add files via upload
    Código completo.

--- lista_tuplas.py
+++ lista_tuplas.py
@@ -1,11 +1,22 @@
 nomes = []
 n = 0
 
-while (n <= 4):
+while (n < 5):
     try:
         nome = input ('Digite um nome: ')
         nomes.append(nome)
         n += 1
+        
     except:
         print ('Digite um nome, não números.')
+        break
+
+print (nomes)
+
+try:
+    remova = nomes.remove(input ('Escolha um nome para ser removido da lista.'))
+
+except:
+    print ('Você não escreveu um nome que esteja na lista')
+
 print (nomes)
